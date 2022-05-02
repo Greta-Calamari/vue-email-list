56e5db9246f57b7d2b38cf03fa7cd120fa095516
ladykakki <greta_calamari@hotmail.it>
2022-05-02 15:18:46 +0200

    ..email

--- script.js
+++ script.js
@@ -7,16 +7,24 @@ const app = new Vue({
 
     },
     methods:{
+        random(){
+            axios.get('https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=10&items=10').then(()=>{
+                
+            })
+        }
+        
 
     },
     created(){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
             for(let i=0; i < 10; i++){
                 this.email = res.data.response
+                console.log(res.data.response)
+                
+            }
 
                 
 
-            }
 
 
         }).catch((error)=>{
